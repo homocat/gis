28f397de9ee66ebda603e49ca09c81fcc68f4f05
NeilianAry <luodan_0464@qq.com>
2024-01-23 22:30:57 +0800

    docs(util, plugin): add jsdoc

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,5 +1,13 @@
 import router from "../router";
 
+
+/**
+ * 注册中间件
+ * @date 1/23/2024 - 10:18:58 PM
+ *
+ * @export
+ * @param {vue root} app
+ */
 export function registerPlugin(app) {
   app
     .use(router)
--- src/util/initViewer.js
+++ src/util/initViewer.js
@@ -3,6 +3,15 @@ import cesiumConfig from "../config/cesium.config"
 
 Cesium.Ion.defaultAccessToken = cesiumConfig.IonDefaultAccessToken
 
+
+/**
+ * 初始化 cesium
+ * @date 1/23/2024 - 10:17:36 PM
+ *
+ * @export
+ * @param {string} [id="cesiumContainer"]
+ * @returns {Cesium.Viewer} viewer
+ */
 export function initViewer(id = "cesiumContainer") {
   const viewer = new Cesium.Viewer(id, {
     infoBox: false,
