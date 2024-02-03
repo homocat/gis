f082c349095714036916d8dd8258bb20416dd9e6
NeilianAry <luodan_0464@qq.com>
2024-02-03 15:03:08 +0800

    fix(util): add parent router

--- src/router/util.js
+++ src/router/util.js
@@ -2,8 +2,8 @@ import demos from "./modules/demos.js"
 import cesium from "./modules/cesium.js"
 
 export default function getNavList() {
-    return {
-        "demosList": [...demos.children],
-        "cesiumList": [...cesium.children]
-    }
+    return [
+        demos,
+        cesium
+    ]
 }
